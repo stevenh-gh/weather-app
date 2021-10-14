c4e87fd6cfc1e2d0f16c48b38ad23cdab9550283
steven <steven.h@live.com>
2021-10-14 15:06:07 -0700

    Call fetch to weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+
+async function getWeather() {
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${process.env.APP_ID}`, { mode: "cors" });
+    const data = await promise.json();
+    console.log(data);
+}
+getWeather();
