f0fef4ce02c992eba9a6781e2db9a822ea162ca7
steven <steven.h@live.com>
2021-10-14 15:29:27 -0700

    Add weather class, and getWeather returns a new weather

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import "./style.css";
 
+class Weather {
+    constructor(city, country, feelsLike, temp, humidity, pressure, windDeg, windSpeed, weatherDesc) {
+        this.city = city;
+        this.country = country;
+        this.feelsLike = feelsLike;
+        this.temp = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.windDeg = windDeg;
+        this.windSpeed = windSpeed;
+        this.weatherDesc = weatherDesc;
+    }
+}
+
 async function getWeather() {
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${process.env.APP_ID}`, { mode: "cors" });
     const data = await promise.json();
-    console.log(data);
+    return new Weather(
+        data.name,
+        data.sys.country,
+        data.main.feels_like,
+        data.main.temp,
+        data.main.humidity,
+        data.main.pressure,
+        data.wind.deg,
+        data.wind.speed,
+        data.weather.description
+    );
 }
 getWeather();
