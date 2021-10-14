22222b993ed1959996a170888c58c2453cc4b6fc
steven <steven.h@live.com>
2021-10-13 22:40:49 -0700

    Setup webpack w/ tailwindcss

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                "style-loader",
+                { loader: "css-loader", options: { importLoaders: 1 } },
+                "postcss-loader"
+            ]
+        }]
+    },
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        open: true
+    }
+};
