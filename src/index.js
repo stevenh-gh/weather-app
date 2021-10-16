a844a494866b61bbe2c1d97fc2a1d566b844746f
steven <steven.h@live.com>
2021-10-15 22:52:38 -0700

    Change console.log result to display results
    Add display function that displays the weather

--- src/index.js
+++ src/index.js
@@ -30,6 +30,30 @@ async function getWeather(city) {
     );
 }
 
+function display(weather) {
+    const result = document.querySelector("#results");
+    while (result.firstChild) { // reset results div
+        result.removeChild(result.firstChild);
+    }
+    result.innerHTML = `
+    <h1>${weather.city}, ${weather.country}</h1>
+    <h2>Feels like:</h2>
+    <p>${weather.feelsLike}</p>
+    <h2>Temperature:</h2>
+    <p>${weather.temp}</h2>
+    <h2>Humidity:</h2>
+    <p>${weather.humidity}</h2>
+    <h2>Pressure:</h2>
+    <p>${weather.pressure}</h2>
+    <h2>Wind degree:</h2>
+    <p>${weather.windDeg}</h2>
+    <h2>Wind speed:</h2>
+    <p>${weather.windSpeed}</h2>
+    <h2>Weather description:</h2>
+    <p>${weather.weatherDesc}</h2>
+    `;
+}
+
 const submitBtn = document.querySelector("button");
 const form = document.querySelector("form");
 submitBtn.addEventListener("click", e => {
@@ -38,6 +62,6 @@ submitBtn.addEventListener("click", e => {
     if (city) {
         e.preventDefault();
         console.log(city);
-        getWeather(city).then(result => console.log(result));
+        getWeather(city).then(result => display(result)).catch(e => console.log(e));
     }
 });
